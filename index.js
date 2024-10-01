a009075e46c2461df20005e3bc7317e69e8cc189
Tyler Stahl <tylerstahl@gmail.com>
2024-09-30 20:52:41 -0400

    initial state assertions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
+const { expect } = require("playwright/test");
 
 async function sortHackerNewsArticles() {
   // launch browser
@@ -9,6 +10,9 @@ async function sortHackerNewsArticles() {
 
   // go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
+
+  await expect(page).toHaveURL("https://news.ycombinator.com/newest");
+  await expect(page.locator(".topsel")).toContainText("new");
 }
 
 (async () => {
