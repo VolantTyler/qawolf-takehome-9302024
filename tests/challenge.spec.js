7252c1229a4a1ade3ba2c170b84a06fb90197cf7
Tyler Stahl <tylerstahl@gmail.com>
2024-10-02 16:00:08 -0400

    timestamp array

--- tests/challenge.spec.js
+++ tests/challenge.spec.js
@@ -2,17 +2,31 @@
 
 import { test, expect } from "@playwright/test";
 
-test("basic validation", async ({ page }) => {
-  await page.goto("https://news.ycombinator.com/newest");
-
-  await expect(page).toHaveURL("https://news.ycombinator.com/newest");
-  await expect(page.locator(".topsel")).toContainText("new");
-});
-
 // validate that EXACTLY the first 100 articles are sorted from newest to oldest.
 test("sorted-ascending", async ({ page }) => {
+  let publishedTimestamps = [];
   await page.goto("https://news.ycombinator.com/newest");
 
-  const ageElements = page.locator(".age").first();
-  console.log(ageElements);
+  const addTimestamps = async (page) => {
+    // get all published timestamps from a page of results
+    const ageElements = await page.locator(".age").all();
+    console.log(ageElements.length);
+
+    for (const post of ageElements) {
+      const timestamp = await post.getAttribute("title");
+      publishedTimestamps.push(timestamp);
+    }
+  };
+
+  await page.goto("https://news.ycombinator.com/newest");
+  await expect(page).toHaveURL("https://news.ycombinator.com/newest");
+  await expect(page.locator(".topsel")).toContainText("new");
+  addTimestamps(page);
+
+  // go to second page of results (results 31-60)
+  await page.getByRole("link", { name: "More" }).locator("nth=-1").click();
+  await expect(page).toHaveURL(/&n=31/);
+  addTimestamps(page);
+
+  console.log(publishedTimestamps, publishedTimestamps.length);
 });
