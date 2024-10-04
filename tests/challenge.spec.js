087430d6db6aa400b7502d2cc9b5a783ac48b03f
Tyler Stahl <tylerstahl@gmail.com>
2024-10-04 12:29:55 -0400

    test: gather 100 timestamps

--- tests/challenge.spec.js
+++ tests/challenge.spec.js
@@ -5,14 +5,13 @@ import { test, expect } from "@playwright/test";
 // validate that EXACTLY the first 100 articles are sorted from newest to oldest.
 test("sorted-ascending", async ({ page }) => {
   let publishedTimestamps = [];
-  await page.goto("https://news.ycombinator.com/newest");
 
   const addTimestamps = async (page) => {
     // get all published timestamps from a page of results
     const ageElements = await page.locator(".age").all();
-    console.log(ageElements.length);
 
     for (const post of ageElements) {
+      if (publishedTimestamps.length === 100) break;
       const timestamp = await post.getAttribute("title");
       publishedTimestamps.push(timestamp);
     }
@@ -21,12 +20,22 @@ test("sorted-ascending", async ({ page }) => {
   await page.goto("https://news.ycombinator.com/newest");
   await expect(page).toHaveURL("https://news.ycombinator.com/newest");
   await expect(page.locator(".topsel")).toContainText("new");
-  addTimestamps(page);
+  await addTimestamps(page);
 
   // go to second page of results (results 31-60)
   await page.getByRole("link", { name: "More" }).locator("nth=-1").click();
   await expect(page).toHaveURL(/&n=31/);
-  addTimestamps(page);
+  await addTimestamps(page);
 
-  console.log(publishedTimestamps, publishedTimestamps.length);
+  // go to third page of results (results 61-90)
+  await page.getByRole("link", { name: "More" }).locator("nth=-1").click();
+  await expect(page).toHaveURL(/&n=61/);
+  await addTimestamps(page);
+
+  // go to fourth page of results (results 91-120)
+  await page.getByRole("link", { name: "More" }).locator("nth=-1").click();
+  await expect(page).toHaveURL(/&n=91/);
+  await addTimestamps(page);
+
+  // console.log(publishedTimestamps, publishedTimestamps.length);
 });
--- tests/test-2.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("test", async ({ page }) => {
-  await page.goto("https://news.ycombinator.com/newest");
-  await page.getByRole("link", { name: "1 minute ago" }).click();
-});
